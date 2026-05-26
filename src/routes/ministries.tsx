68021797f162b4f9b6f5c95b0f96051e64af55d4
dev <dev@local>
2026-05-26 21:46:14 +0200

    Ministries: wider tiles (2-col until xl, then 3-col)

--- src/routes/ministries.tsx
+++ src/routes/ministries.tsx
@@ -32,7 +32,7 @@ function Ministries() {
       />
 
       <section className="mx-auto max-w-6xl px-4 py-16">
-        <div className="grid gap-5 sm:grid-cols-2 lg:grid-cols-3">
+        <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3">
           {ministries.map((m) => {
             const Icon = m.icon;
             return (
